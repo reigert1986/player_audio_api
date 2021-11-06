e1c46227df861e942bebd1d3baf4b10477ed9ec7
reigert1986 <85256672+reigert1986@users.noreply.github.com>
2021-11-06 13:41:56 -0300

    Update au.js

--- js/au.js
+++ js/au.js
@@ -1,182 +1,24 @@
 
-const canciones = [
-  {
-		file : "../audio/Plena Enganchados Uruguay 2021 - Playlist Oficial(MP3_128K)_1.mp3",
-		song : "Plena Enganchados Uruguay 2021"	
-	},
-	{
-		file : "../audio/-- Chillhop Essentials Summer 2018・jazz beats _ chill hiphop(MP3_128K).mp3",
-		song : "Chillhop Essentials Summer 2018・jazz beats"	
-	},
-	{
-		file : "../audio/Flashback Dance Anos 90 - CD Duplo p(2017) High Quality Audio.m4a",
-		song : "Flashback Dance Anos 90"	
-	},
-	{
-		file : "../audio/bmgJazzBossaNova.mp3",
-		song : "bmg Jazz Bossa Nova"	
-	},
-	{
-		file : "../audio/Best of Jazz Fusion and Funk Music - Too Hot Too Tight(MP3_128K).mp3",
-		song : "Too Hot Too Tight"	
-	},
-	{
-		file : "../audio/Buddha Lounge Classics Compilation - more than 5 hours non stop(MP3_128K).mp3",
-		song : "Buddha Lounge Classics Compilation"	
-	},
-	{
-		file : "../audio/Top House Music - Late Night In New York(MP3_128K).mp3",
-		song : "Top House Music - Late Night In New York"	
-	},
-	{
-		file : "../audio/Soda Stereo - Doble Vida (1988) (Full Album)(MP3_128K)_1.mp3",
-		song : "Soda Stereo - Doble Vida (1988)"	
-	},
-	{
-		file : "../audio/Ir̲o̲n B̲utte̲rfly̲ - H̲e̲avy (Full Album) 1968.mp3",
-		song : "Iron Butterfly - Heavy 1968"	
-	},
-	{
-		file : "../audio/I̤r̤o̤n̤ ̤B̤ṳt̤t̤e̤r̤f̤l̤y̤-̤B̤a̤l̤l̤-- Full Album HQ 1969.mp3",
-		song : "I̤r̤o̤n̤ ̤B̤ṳt̤t̤e̤r̤f̤l̤y̤ -̤ B̤a̤l̤l̤ 1969"	
-	},
-	{
-		file : "../audio/I̤͟͟r̤͟͟o̤͟n  B̤͟͟ṳ͟͟tterfly  M͟etam͟o͟rphosis- Full Album 1970.mp3",
-		song : "Iron Butterfly  M͟etam͟o͟rphosis 1970"	
-	},
-	{
-		file : "../audio/Let_s Work with Reggae Dub - Relaxing Sound - Relaxing Sound(MP3_128K).mp3",
-		song : "Let_s Work with Reggae Dub"	
-	},
-	{
-		file : "../audio/Eraldo Volonté Eraldo Volonté Presenta Jazz (Now) In Italy [1966 - Album].mp3",
-		song : "Eraldo Volonté Eraldo Volonté Presenta Jazz (Now) In Italy "	
-	},
-	{
-		file : "../audio/Eraldo Volonté Quartet – Safari (1975 - Album).mp3",
-		song : "Eraldo Volonté Quartet – Safari 1975 "	
-	},
-	{
-		file : "../audio/Don Patterson & Booker Ervin - Legends of Acid Jazz (1996 - Compilation).mp3",
-		song : "Don Patterson & Booker Ervin - Legends of Acid Jazz 1996 "	
-	},
-	{
-		file : "../audio/Incognito - In Concert   Ohne Filter  (Full Concert).mp3",
-		song : "Incognito - In Concert Ohne Filter.mp3"	
-	},
-	{
-		file : "../audio/Morcheeba - Opus Café - FULL LIVE HD.mp3",
-		song : "Morcheeba - Opus Café"	
-	},
-	{
-		file : "../audio/Let_s Work with Soul R_B - Relaxing Sound(MP3_128K).mp3",
-		song : "Let_s Work with Soul R_B "	
-	},
-	{
-		file : "../audio/Funky Disco vinyl mix _ Kirollus(MP3_128K).mp3",
-		song : "Funky Disco vinyl mix"	
-	},
-	{
-		file : "../audio/Art Of Melodic Techno Astronauts Trip By Patrick Slayer.mp3",
-		song : "Art Of Melodic Techno Astronauts Trip By Patrick Slayer"	
-	},
-	{
-		file : "../audio/Jamie Jones - DJ set from  Paradise at DC10 Terrace Ibiza(MP3_128K)_1.mp3",
-		song : "Jamie Jones - DJ set from  Paradise at DC10 Terrace Ibiza"	
-	},
-	{
-		file : "../audio/JAMIE JONES at Music On Festival 2019(MP3_128K)_1.mp3",
-		song : "JAMIE JONES at Music On Festival 2019"	
-	},
-	{
-		file : "../audio/Regen & Gewittergeräusche, Natur & Regengeräusche zum Entspannen, Meditieren, Lernen & Einschlafen.mp3",
-		song : "Regen & Gewittergeräusche, Natur & Regengeräusche zum Entspannen, Meditieren, Lernen & Einschlafen"	
-	},
-	{
-		file : "../audio/Treasures From the Temple.mp3",
-		song : "Treasures From the Temple"	
-	},
-	{
-		file : "../audio/The Best of Dance Music - Funky House For A Summer Trip(MP3_128K).mp3",
-		song : "The Best of Dance Music - Funky House For A Summer Trip"	
-	},
-	{
-		file : "../audio/Stone Temple Pilots - Core (Full Album).mp3",
-		song : "Stone Temple Pilots - Core"	
-	},
-	{
-		file : "../audio/Korn - Full Concert - 07_23_99 - Woodstock 99 East Stage (OFFICIAL)(MP3_128K)_1.mp3",
-		song : "Korn - 07_23_99 - Woodstock 99 East Stage"	
-	},
-	{
-		file : "../audio/AC_DC - Live at Caste Donington, England, August 17, 1991 (Full concert - HD 50fps).mp3",
-		song : "AC_DC - Live at Caste Donington, England, August 17, 1991"	
-	},
-	{
-		file : "../audio/Jamiroquai - Live at North Sea Jazz Festival - 1995_7_14 [Full Show].mp3",
-		song : "Jamiroquai - Live at North Sea Jazz Festival - 1995_7_14 "	
-	},
-	{
-		file : "../audio/Janis Joplin _ Live in Frankfurt_ Germany (RARE Concert Footage)(MP3_128K)_1.mp3",
-		song : "Janis Joplin _ Live in Frankfurt_ Germany"	
-	},
-	{
-		file : "../audio/Funky Disco vinyl mix _ Kirollus(MP3_128K).mp3",
-		song : "Funky Disco vinyl mix"	
-	},
-	{
-		file : "../audio/Brubeck Quartet Time Out Album.mp3",
-		song : "Brubeck Quartet Time Out 1959"	
-	},
-	{
-		file : "../audio/70s Japanese Jazz Mix Vol.1.mp3",
-		song : "70s Japanese Jazz Mix Vol.1"	
-	},
-	{
-		file : "../audio/70s Japanese Jazz Mix Vol.2.mp3",
-		song : "70s Japanese Jazz Mix Vol.2.mp3"	
-	},
-	{
-		file : "../audio/70s Japanese Jazz Mix Vol.1.mp3",
-		song : "70s Japanese Jazz Mix Vol.3.mp3"	
-	},
-	{
-		file : "../audio/Fiona Apple - Tidal (Full Album).mp3",
-		song : "Fiona Apple - Tidal"	
-	},
-	{
-		file : "../audio/Fiona Apple - Skeleton Songs (Full Bootleg)(MP3_128K)_1.mp3",
-		song : "Fiona Apple - Skeleton Songs (Full Bootleg)"	
-	},
-	{
-		file : "../audio/Fiona Apple - Fetch the Bolt Cutters (Full Album).mp3",
-		song : "Fiona Apple - Fetch the Bolt Cutters"	
-	},
-	{
-		file : "../audio/Music to put you in a better mood _ lofi _ relax _ stress relief(MP3_128K).mp3",
-		song : "Music to put you in a better mood _ lofi _ relax _ stress relief"	
-	},
-	{
-		file : "../audio/Música Para Estudiar y Relajarse 📒 Lofi-Hip Hop.mp3",
-		song : "Música Para Estudiar y Relajarse 📒 Lofi-Hip Hop"	
-	},
-	{
-		file : "../audio/Night Chill_ Night City Hip Hop Jazz - Lofi Jazzhop Radio for Study(MP3_128K).mp3",
-		song : "FNight Chill_ Night City Hip Hop Jazz - Lofi Jazzhop Radio for Study"	
-	},
-	{
-		file : "../audio/Chill Drive - Lofi hip hop mix _ Stress Relief_ Relaxing Music(MP3_128K).mp3",
-		song : "Chill Drive - Lofi hip hop mix _ Stress Relief_ Relaxing Music"	
-	},
-	{
-		file : "../audio/Jonas Kopp - Live _ Episode_1 - Club Room (Ex.Fabrica) Santiago de Chile (10.06.2017)(MP3_128K).mp3",
-		song : "Jonas Kopp - Live _ Episode_1 - Club Room (Ex.Fabrica) Santiago de Chile (10.06.2017)"	
-	},
-	
-	
-		
-]
 
+const canciones = [
+	{
+		file : "./audio/pista1.mp3",
+		song : "pista1"
+	},
+	{
+		file : "./audio/pista2.mp3",
+		song : "pista2"
+	},
+	{
+		file : "./audio/pista3.mp3",
+		song : "pista3"
+	},
+	{
+		file : "./audio/pista4.mp3",
+		song : "pista4"
+	},
+	
+]
 
 
 
